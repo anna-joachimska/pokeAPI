7cfe795e47538e6a17d9b21366fc4f9d0c9abe1b
anna_joachimska <ajoachimska@bee2code.com>
2023-01-11 08:45:09 +0100

    sequelize - connection to db

--- db.js
+++ db.js
@@ -1,19 +1,7 @@
 const Sequelize = require("sequelize");
-// const Pool = require("pg").Pool;
 require('dotenv').config();
-// //
-// const pool = new Pool({
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     user: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD,
-//     database: process.env.PG_DATABASE,
-//     client : null,
-// });
-//
-// module.exports = pool;
-//
-const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {host: 'localhost', dialect: 'postgres', logging: (...msg) => console.log(msg)});
+
+const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {host: 'localhost', dialect: 'postgres', });//logging: (...msg) => console.log(msg)});
     try {
         sequelize.authenticate();
         console.log('Connection has been established successfully.');

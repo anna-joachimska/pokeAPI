703db74cbf8ecb878ec86e0173fd85440ebebc77
anna_joachimska <ajoachimska@bee2code.com>
2023-01-11 08:45:32 +0100

    fix routes and added sorting endpoints

--- routes/abilities.js
+++ routes/abilities.js
@@ -4,12 +4,20 @@ const abilityController = require("../controllers/abiliyController");
 
 router.get('/', abilityController.getAllAbilities);
 
+router.get('/sorted/byId/asc', abilityController.getAllAbilitiesSortedByIdASC);
+
+router.get('/sorted/byId/desc', abilityController.getAllAbilitiesSortedByIdDESC);
+
+router.get('/sorted/byName', abilityController.getAllAbilitiesSortedByName);
+
+router.get('/withPokemons', abilityController.getAllAbilitiesWithPokemons);
+
 router.post('/', abilityController.createNewAbility);
 
-router.get('/ability/:abilityId', abilityController.getAbility);
+router.get('/:abilityId', abilityController.getAbility);
 
-router.put('/ability/:abilityId', abilityController.updateAbility);
+router.put('/:abilityId', abilityController.updateAbility);
 
-router.delete('/ability/:abilityId', abilityController.deleteAbility);
+router.delete('/:abilityId', abilityController.deleteAbility);
 
 module.exports = router;
